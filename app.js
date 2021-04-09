1fbc6cd676ca4693bfa034aabb41f146a80bc0e6
Ritvick-me <pandey.ritvick.v@gmail.com>
2021-04-09 18:10:13 +0530

    add scripts

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ const mailer = require("./PortfolioEnquiryFormDetails");
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
