c6a201283bfffadd7263043d8f123b63dfa8d978
Ritvick-me <pandey.ritvick.v@gmail.com>
2021-04-09 16:35:49 +0530

    add cors options

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const mailer = require("./PortfolioEnquiryFormDetails");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server connected to " + (process.env.PORT || 5000));
+app.listen(process.env.PORT || 5000 || 3000, () => {
+  console.log("Server connected to " + (process.env.PORT || 5000 || 3000));
 });
 
 app.use("/PortfolioEnquiryFormDetails", mailer);
