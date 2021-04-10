4988a5702a34cf72d1ddc99949a12aa5b3cac15a
Ritvick-me <pandey.ritvick.v@gmail.com>
2021-04-11 01:56:38 +0530

    feat(add-server-side-validation): add server side form validation and reCaptcha verification

--- PortfolioEnquiryFormDetails.js
+++ PortfolioEnquiryFormDetails.js
@@ -1,6 +1,7 @@
+const axios = require("axios");
+const validator = require("email-validator");
 const express = require("express");
 const nodemailer = require("nodemailer");
-
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
@@ -11,43 +12,128 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const verifyReCaptcha = async (token) => {
+  const secret = process.env.RECAPTCHA_SECRET;
+  const response = await axios({
+    url: `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    method: "post",
+  });
+
+  return response.data.success;
+};
+
+const validateEmptyDates = (date1, date2, date3) => {
+  if (!date1 || !date2 || !date3) return false;
+  return true;
+};
+
+const validateTwinningSlots = (date1, date2, date3, time1, time2, time3) => {
+  let repititiveData = {};
+  let key1 = date1 + time1;
+  let key2 = date2 + time2;
+  let key3 = date3 + time3;
+  repititiveData[key1] = 1;
+  if (repititiveData[key2]) {
+    return false;
+  } else repititiveData[key2] = 1;
+  if (repititiveData[key3]) {
+    return false;
+  }
+  return true;
+};
+
+const emailValidator = (email) => {
+  return validator.validate(email);
+};
+
 router.post("/", async (req, res) => {
-  const mailBody = req.body;
+  const human = await verifyReCaptcha(req.headers["x-recaptcha-token"]);
 
-  const mailOptions = {
-    from: mailBody.email,
-    to: process.env.EMAIL,
-    subject: "Appointment request from " + mailBody.name,
-    text:
-      "Supplicant's name: " +
-      mailBody.name +
-      "\nSupplicant's Email: " +
-      mailBody.email +
-      "\n\nPreference 1: \n\tDate:" +
-      mailBody.date1 +
-      "\tTime: " +
-      mailBody.time1 +
-      "\n\nPreference 1: \n\tDate:" +
-      mailBody.date2 +
-      "\tTime: " +
-      mailBody.time2 +
-      "\n\nPreference 1: \n\tDate:" +
-      mailBody.date3 +
-      "\tTime: " +
-      mailBody.time3 +
-      "\n\nTopic: " +
-      mailBody.topic,
-  };
+  if (!human) {
+    res.status(400).json({ status: false });
+    return;
+  }
 
-  await transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log("Mail could not be sent...\n", err);
-      res.status(500).json({ status: false });
+  try {
+    const mailBody = req.body;
+
+    let isEmptyDate = validateEmptyDates(
+      mailBody.date1,
+      mailBody.date2,
+      mailBody.date3
+    );
+    if (!isEmptyDate) {
+      res.status(400).json({ status: false });
       return;
     }
-    console.log("Mail was sent...");
-    res.status(202).json({ status: true });
-  });
+
+    let isSlotTwinning = validateTwinningSlots(
+      mailBody.date1,
+      mailBody.date2,
+      mailBody.date3,
+      mailBody.time1,
+      mailBody.time2,
+      mailBody.time3
+    );
+    if (!isSlotTwinning) {
+      res.status(400).json({ status: false });
+      return;
+    }
+
+    if (!mailBody.name) {
+      res.status(400).json({ status: false });
+      return;
+    }
+
+    let isEmail = emailValidator(mailBody.email);
+    if (!isEmail) {
+      res.status(400).json({ status: false });
+      return;
+    }
+
+    if (!mailBody.topic) {
+      res.status(400).json({ status: false });
+      return;
+    }
+
+    const mailOptions = {
+      from: mailBody.email,
+      to: process.env.EMAIL,
+      subject: "Appointment request from " + mailBody.name,
+      text:
+        "Supplicant's name: " +
+        mailBody.name +
+        "\nSupplicant's Email: " +
+        mailBody.email +
+        "\n\nPreference 1: \n\tDate:" +
+        mailBody.date1 +
+        "\tTime: " +
+        mailBody.time1 +
+        "\n\nPreference 1: \n\tDate:" +
+        mailBody.date2 +
+        "\tTime: " +
+        mailBody.time2 +
+        "\n\nPreference 1: \n\tDate:" +
+        mailBody.date3 +
+        "\tTime: " +
+        mailBody.time3 +
+        "\n\nTopic: " +
+        mailBody.topic,
+    };
+
+    await transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log("Mail could not be sent...\n", err);
+        res.status(500).json({ status: false });
+        return;
+      }
+      console.log("Mail was sent...");
+      res.status(202).json({ status: true });
+    });
+  } catch {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(process.env.PORT || 5000 || 3000, () => {
-  console.log("Server connected to " + (process.env.PORT || 5000 || 3000));
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server connected to " + (process.env.PORT || 5000));
 });
 
 app.use("/PortfolioEnquiryFormDetails", mailer);
